c0f1ef048bb3cd1ed9a53e31342cd59c6f612ca2
brunosaboya <brunobs6@al.insper.edu.br>
2021-11-07 14:28:04 -0300

    Added a Model & Superuser

---
+++ proj2_backend/weather_api/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+DESCRIPTIONS = (
+    (0, "Sunny"),
+    (1, "Rain"),
+    (2, "Cloudy"),
+    (4, "Snow")
+)
+
+class Description(models.Model):
+    description = models.IntegerField(choices=DESCRIPTIONS, default=0)
+    created_on = models.DateTimeField(auto_now_add = True)
+    temperature = models.FloatField()
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return str(self.created_on)
